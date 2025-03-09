80b6c154bff2b09763b757a65d5c4e0a2cc18ed8
Lachezar Donchev <luchko2006@abv.bg>
2025-03-09 12:54:05 +0200

    Update Program.cs

--- FlightManager/Program.cs
+++ FlightManager/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using FlightManager.Data;
+using FlightManager.Models;
 
 namespace FlightManager;
 
@@ -16,7 +17,7 @@ public class Program
             options.UseSqlServer(connectionString));
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-        builder.Services.AddDefaultIdentity<IdentityUser>()
+        builder.Services.AddDefaultIdentity<AppUser>()
             .AddEntityFrameworkStores<ApplicationDbContext>();
         builder.Services.AddControllersWithViews();
 
